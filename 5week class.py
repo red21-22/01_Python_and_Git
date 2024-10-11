7ec95abbf66ab067af36f5d649a9b8821a072b35
red21-22 <red103198@naver.com>
2024-10-11 22:15:21 +0900

    6week class 수정본
    6week class 수정본

--- 5week class.py
+++ 5week class.py
@@ -11,7 +11,16 @@ class new_Id:
         print(f"전화번호: {self.phone}")
 
 
-membership = new_Id("계정 이름은 김도영이며", "나이는23이고","번호는 010-3376-8649입니다")
+membership = new_Id("계정 이름은 김도영이며", "나이는23이고", "번호는 010-3376-8649입니다")
 membership.information()
 
 #Quiz 2
+def check_length_of_message(message):
+    if message > 20:
+        return "100원"
+    else:
+        return "50원"
+
+user_input_message = input("문자를 입력하세요")
+length = len(user_input_message)
+print(check_length_of_message(length))
